5e6ecd423df2ea910d33b5a548d4295ba90a5e0b
Harry-Bro <likeharrybro@gmail.com>
2021-09-20 11:23:13 +0900

    :sparkles: Add post and put mapping controller

--- basic-mvc/src/main/kotlin/com/harrybro/basicmvc/api/TestApiController.kt
+++ basic-mvc/src/main/kotlin/com/harrybro/basicmvc/api/TestApiController.kt
@@ -1,6 +1,7 @@
 package com.harrybro.basicmvc.api
 
 import com.harrybro.basicmvc.dto.UserRequestDto
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -14,7 +15,7 @@ class TestApiController {
     }
 
     @GetMapping("/hello")   // http://localhost:8080/api/hello?item=harry&item=bro
-    fun getArrayParam(@RequestParam item: Array<String>): ResponseEntity<Array<String>>{
+    fun getArrayParam(@RequestParam item: Array<String>): ResponseEntity<Array<String>> {
         return ResponseEntity.ok(item)
     }
 
@@ -22,6 +23,17 @@ class TestApiController {
     fun getBody(request: UserRequestDto): ResponseEntity<UserRequestDto> {
         println(request)
         return ResponseEntity.ok(request);
+    }
+
+    @PostMapping("users")
+    fun userSave(@RequestBody request: UserRequestDto): ResponseEntity<UserRequestDto> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(request)
+    }
+
+    @PutMapping("users/{id}")
+    fun userUpdate(@PathVariable id:String): ResponseEntity<String> {
+        println(id)
+        return ResponseEntity.ok("Success update user.")
     }
 
 }
--- basic-mvc/src/main/kotlin/com/harrybro/basicmvc/dto/UserRequestDto.kt
+++ basic-mvc/src/main/kotlin/com/harrybro/basicmvc/dto/UserRequestDto.kt
@@ -1,8 +1,14 @@
 package com.harrybro.basicmvc.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
+//@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequestDto(
     var name: String? = null,
     var age: Int? = null,
     var email: String? = null,
-    var address: String? = null
+    var address: String? = null,
+
+    @JsonProperty("phone_number")
+    var phoneNumber: String? = null
 )
